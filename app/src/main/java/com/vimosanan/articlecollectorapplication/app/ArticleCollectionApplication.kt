ccea008f8910c444b3929b53d73c4e452f777a0e
vimosanana <vimosanana@ellipsis.lk>
2020-06-04 11:48:54 +0800

    add project structure

---
+++ app/src/main/java/com/vimosanan/articlecollectorapplication/app/ArticleCollectionApplication.kt
@@ -0,0 +1,6 @@
+package com.vimosanan.articlecollectorapplication.app
+
+import android.app.Application
+
+class ArticleCollectionApplication: Application() {
+}
---
+++ app/src/main/java/com/vimosanan/articlecollectorapplication/app/Constants.kt
@@ -0,0 +1,2 @@
+package com.vimosanan.articlecollectorapplication.app
+
